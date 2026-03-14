7b36d4dbdeaf5cffc1e39df263dabe823526ba21
felekez <terefefeleke@gmail.com>
2026-03-14 12:31:04 +0300

    Add a category to the product model

--- backend/store/models.py
+++ backend/store/models.py
@@ -27,6 +27,7 @@ class Category(models.Model):
 #         return self.name
 
 class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
